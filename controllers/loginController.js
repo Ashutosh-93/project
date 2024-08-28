8b7e130f4a5e3d1e23a1e6a10f251edc9090df80
Ashutosh-93 <ashutosh69695@gmail.com>
2024-08-28 19:03:01 +0530

    changes

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,16 +1,17 @@
+require('dotenv').config();
 const { findOne } = require('../models/product');
 const userModel = require('../models/user');
 const {hashPassword,comparePassword} = require('../utils/bcryptUtils');
 const jwt = require('jsonwebtoken');
-require('dotenv').config();
 
 const loginController = async (req,res)=>{
     let {email,password} = req.body;
-    let user = findOne({email:email});
-    if(!email||!comparePassword(password,user.password)){
+    let user = await findOne({email:email});
+    let result = await comparePassword(password,user.password);
+    if(!user||!result){
         return res.status(401).json({error:"Invalid email or password"});
     }
-    const token = jwt.sign({email:user.email,id:user._id});
+    const token = jwt.sign({email:user.email,id:user._id},"secret");
     req.cookie('token',token);
     res.send("logged in");
 
